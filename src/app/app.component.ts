7bf81908102e500f63a95f89c87b1f542696ee4e
unknown <phaser005>
2021-08-26 20:12:30 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicioClase01';
+  saludar(){
+    console.log("Hola 4B!");
+  }
 }
---
+++ src/app/components/welcomme/welcomme.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WelcommeComponent } from './welcomme.component';
+
+describe('WelcommeComponent', () => {
+  let component: WelcommeComponent;
+  let fixture: ComponentFixture<WelcommeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WelcommeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WelcommeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
